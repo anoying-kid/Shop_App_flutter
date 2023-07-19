8539d398487436a2f016051541ff61710fec5eea
anoying-kid <yugrana853@gmail.com>
2023-07-19 12:15:46 +0530

    added image size in appbar of the product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -13,27 +13,35 @@ class ProductDetailScreen extends StatelessWidget {
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-        appBar: AppBar(title: Text(loadedProduct.title)),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 300,
-                width: double.infinity,
-                child: Hero(
-                  tag: loadedProduct.id,
-                  child: Image.network(
-                    loadedProduct.imageUrl,
-                    fit: BoxFit.contain,
-                  ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
                 ),
               ),
+            ),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate(
+            [
               const SizedBox(
                 height: 10,
               ),
               Text(
                 '\$${loadedProduct.price}',
-                style: const TextStyle(color: Colors.grey, fontSize: 20),
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
               ),
               const SizedBox(
                 height: 10,
@@ -47,7 +55,9 @@ class ProductDetailScreen extends StatelessWidget {
                     softWrap: true,
                   ))
             ],
-          ),
-        ));
+          ))
+        ],
+      ),
+    );
   }
 }
